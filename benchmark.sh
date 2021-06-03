599d086125c81d45e7a6aa2be99c3030440d5390
John Reitano <jreitano@gmail.com>
2021-06-03 14:17:47 -0700

    source dev.env from local mmhmm-service directory in script

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# making the assumption that this repo located in the same parent director as mmhmm-service
+source ../mmhmm-service/dev.env
 go test -test.bench=. -test.benchmem | prettybench
